ece0c95839bd1d63e1c84e9c9984ef5647569c92
Teri Walker <teri.m.walker@gmail.com>
2023-12-18 00:32:02 -0500

    create herlper files

---
+++ enable SSH.ps1
@@ -0,0 +1,22 @@
+
+Function fn_EnableSSHand
+Shell {
+
+$host_list = Get-Content "host_list.txt"
+
+
+Connect-VIServer $vcenter 
+
+#enable SSH
+foreach($hosts in $host_list){
+	Get-VMHostService -VMHost $hosts | Where-Object {$_.Key -eq "TSM-SSH" } | Start-VMHostService -confirm:$false 
+}
+
+#enable bash shell
+foreach($hosts in $host_list){
+	Get-VMHostService -VMHost $hosts | Where-Object {$_.Key -eq "TSM" } | Start-VMHostService -confirm:$false 
+}
+
+Disconnect-VIServer * -confirm:$false 
+
+fn_EnableSSHandShell
