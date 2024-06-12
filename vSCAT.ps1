b672cbcba68ee26c091780ecc5fe498337581d9c
Teri Walker <teri.walker@broadcom.com>
2024-06-12 09:29:18 -0400

    update vsphere scanner function

--- vSCAT.ps1
+++ vSCAT.ps1
@@ -120,16 +120,18 @@ Function fn_sddcscanner {
       $jsonOutput = "/root/results/ESX_Scan_"+$global:defaultVIServer+"_"+$global:date+".json"
     Write-Host "Saving results to: "$jsonOutput
     $profilePath ="/root/dod-compliance-and-automation/vsphere/"+$global:vCVersion[0]+".0/vsphere/inspec/vmware-vsphere-"+$global:vCVersion[0]+".0-stig-baseline/esxi"
-  
-    <#$command ="inspec exec "+$profilePath+ " --show-progress -t ssh://"+$global:VCuser+"@"+$global:defaultVIServer+" --password "+$global:VCpass+"--input-file"+ $profilePath+"inspec.yml --show-progress --reporter=cli json:"+$jsonOutput  
+  <#$command ="inspec exec "+$profilePath+ " --show-progress -t ssh://"+$global:VCuser+"@"+$global:defaultVIServer+" --password "+$global:VCpass+"--input-file"+ $profilePath+"inspec.yml --show-progress --reporter=cli json:"+$jsonOutput  
     #>
     $command ="inspec exec $profilePath/. -t vmware:// --input-file $profilePath/inspec.yml --show-progress --reporter=cli json:$jsonOutput" 
    
     Write-Host "The command I'm sending is "
     Write-Host $command
-    fn_PressAnyKey
-    
-    Invoke-Expression $command  Function fn_vSphereScanner { 
+    fn_PressAnyKey    
+    Invoke-Expression $command  
+     Write-Host "ESXi Host Scan Complete!"  
+  }
+
+  Function fn_vSphereScanner { 
     Write-Host "Running vSphere (vCenter, ESXi Host, and Virtual Machine) Scan:"
       $env:VISERVER=$global:defaultVIServer
       $env:VISERVER_USERNAME=$global:VCuser
@@ -142,16 +144,9 @@ Function fn_sddcscanner {
     Write-Host "The command I'm sending is "
     Write-Host $command
     fn_PressAnyKey  
-
     Invoke-Expression $command
     Write-Host "vSphere Scan Complete!"
   }
-    Write-Host "ESXi Host Scan Complete!"
-  }
-
-
-
-
   Function fn_VMscanner { 
     Write-Host "Running ESX Host Scan:"
       $env:VISERVER=$global:defaultVIServer
