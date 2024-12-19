1960d3ea52c6a0853f303afda4c7ec5f11af95ef
Teri Walker <teri.walker@broadcom.com>
2024-12-19 10:27:39 -0500

    fixed loops

--- Helper Scripts/sshdisabletimeout.ps1
+++ Helper Scripts/sshdisabletimeout.ps1
@@ -1,12 +1,35 @@
-write a powershell script to disable ssh timeout in vcenter
-# Connect to vCenter Server
-Connect-VIServer -Server <vCenter_Server_FQDN> -User <username> -Password <password>
+Powershell script to disable ssh timeout in vcenter
 
-# Disable SSH timeout
-$esxiShellTimeout = Get-VMHostAdvancedSetting -Name UserVars.ESXiShellTimeOut
-$esxiShellTimeout.Value = 0
-Set-VMHostAdvancedSetting -Entity $esxiShellTimeout.Entity -Name $esxiShellTimeout.Name -Value 0
+Function fn_disableSSHtimeout {
 
+    Write-Host -ForegroundColor RED "If you haven't created a host_list.txt file, stop now and create"
+	$host_list = Get-Content "host_list.txt"
 
-#disable ssh in vcsa
-#/etc/profile.d/tmout.sh
+    # Connect to vCenter Server
+    $vcenter = Read-Host -Prompt "Enter vCenter you want to connect to"
+	Connect-VIServer $vcenter 
+
+    # Disable SSH shell timeout for each host in host_list.txt
+    foreach($hosts in $host_list){
+		Write-Host -ForegroundColor GREEN "Modifying shell timeout for  " -NoNewline
+		Write-Host -ForegroundColor YELLOW "$VMhost"		
+	    $esxiShellTimeout = Get-VMHostAdvancedSetting -Name UserVars.ESXiShellTimeOut
+        $esxiShellTimeout.Value = 0
+        Get-VMHost | Set-VMHostAdvancedSetting -Entity $esxiShellTimeout.Entity -Name $esxiShellTimeout.Name -Value 0 -Confirm:$false
+    }
+
+    #disable ssh in vcsa
+    #/etc/profile.d/tmout.sh
+
+    #Disable interactive shell timeout for each host in host_list.txt
+    foreach($hosts in $host_list){
+		Write-Host -ForegroundColor GREEN "Modifying shell interactive timeout for  " -NoNewline
+		Write-Host -ForegroundColor YELLOW "$VMhost"	
+        Get-VMHost | Get-AdvancedSetting -Name UserVars.ESXiShellInteractiveTimeOut | Set-AdvancedSetting -Value 0 -Confirm:$false
+    }
+    
+  Disconnect-VIServer -Server $vcenter -Confirm:$false
+}
+    
+
+fn_disableSSHtimeout
