9982ab8a25b03dbd60a91637386a550d7903b072
Teri Walker <teri.walker@broadcom.com>
2025-02-24 15:10:27 -0600

    update

--- Helper Scripts/enableSSH.ps1
+++ Helper Scripts/enableSSH.ps1
@@ -9,16 +9,16 @@ Function fn_EnableSSHandShell {
 	Write-Host -ForegroundColor GREEN "Do you want to enable SSH and Shell on all ESXi hosts in the vCenter? (Y/N)"
 	$answer = Read-Host
 	if($answer -eq "N"){
-		Write-Host -ForegroundColor RED "What host do you want to enable SSH and Shell on?"
-		$hosts = Read-Host
+		$host_list = Read-Host -Prompt -ForegroundColor RED "What host do you want to enable SSH and Shell on?"
 		exit
 	}
 	if($answer -eq "Y"){
 		Write-Host -ForegroundColor GREEN "Creating the host_list.txt file of all ESXi hosts in the vCenter..."
+		Get-VMHost | Select Name | Out-File host_list.txt
+		$host_list = Get-Content "host_list.txt"
 	}
 
-	Get-VMHost | Select Name | Out-File host_list.txt
-		$host_list = Get-Content "host_list.txt"
+	
 	
 	#enable SSH, do not prompt for user confirmation (-confirm:$false)
 	foreach($hosts in $host_list){
